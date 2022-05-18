d3c320bda67089e7e46f39db5a96901eaa44721d
Adarsh Goel <88939380+adarsh-goel@users.noreply.github.com>
2022-05-18 14:09:24 +0530

    v1

---
+++ web/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./screens/Home";
+import DriveHelp from "./screens/DriveHelp";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/drive/:folderId?">
+          <Home tab="drive" />
+        </Route>
+        <Route exact path="/download">
+          <Home tab="downloads" />
+        </Route>
+        <Route exact path="/drivehelp" component={DriveHelp} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ web/src/screens/Home.js
@@ -0,0 +1,23 @@
+import React, { lazy, Suspense } from "react";
+import TopNav from "../components/TopNav";
+
+const Downloads = lazy(() => import("../screens/Downloads"));
+const Drive = lazy(() => import("../screens/Drive"));
+
+export default function Home({ tab }) {
+  const nav = tab || "search";
+
+  return (
+    <>
+      <TopNav nav={nav} />
+      <main>
+        <div className="content">
+          <Suspense fallback={<div className="div-loading" />}>
+            {nav === "downloads" && <Downloads />}
+            {nav === "drive" && <Drive />}
+          </Suspense>
+        </div>
+      </main>
+    </>
+  );
+}
